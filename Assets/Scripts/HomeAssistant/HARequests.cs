8833215d94c25a4e9f1a57f6695e407316337d0a
Jakub Olejnik <jaolejnik@proton.me>
2022-11-27 18:07:51 +0100

    Added Spotify integration

--- Assets/Scripts/HomeAssistant/HARequests.cs
+++ Assets/Scripts/HomeAssistant/HARequests.cs
@@ -8,19 +8,25 @@ public class HARequests : MonoBehaviour
     public string haUrl;
     public string apiKey;
 
-    void Awake()
-    {
-        RestClient.DefaultRequestHeaders["Authorization"] = $"Bearer {apiKey}";
-    }
-
+    // TODO CHANGE DICT TO JOBJECT
     public RSG.IPromise<ResponseHelper> Post(string media, string action, Dictionary<string, dynamic> objectData)
     {
-        return RestClient.Post($"{haUrl}/api/services/{media}/{action}", JsonConvert.SerializeObject(objectData));
+        RestClient.DefaultRequestHeaders["Authorization"] = $"Bearer {apiKey}";
+
+        var resPromise = RestClient.Post($"{haUrl}/api/services/{media}/{action}", JsonConvert.SerializeObject(objectData));
+        RestClient.ClearDefaultHeaders();
+
+        return resPromise;
     }
 
     public RSG.IPromise<ResponseHelper> Get(string mediaId, Dictionary<string, dynamic> dataToSave)
     {
-        return RestClient.Get($"{haUrl}/api/states/{mediaId}");
+        RestClient.DefaultRequestHeaders["Authorization"] = $"Bearer {apiKey}";
+
+        var resPromise = RestClient.Get($"{haUrl}/api/states/{mediaId}");
+        RestClient.ClearDefaultHeaders();
+
+        return resPromise;
     }
 }
 
--- Assets/Scripts/Sonos/SonosController.cs
+++ Assets/Scripts/Sonos/SonosController.cs
@@ -2,33 +2,41 @@ using UnityEngine;
 using System;
 using TMPro;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System.Text;
+using Newtonsoft.Json.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using Microsoft.MixedReality.Toolkit.UI;
+using MusicHelpers;
 
-struct Track
-{
-    public string title;
-    public string artist;
-    public int duration;
-};
+
+// TODO CHANGE DICT TO JOBJECT
 
 public class SonosController : MonoBehaviour
 {
     public HARequests HUB;
+    public SpotifyAPI spotify;
     public string id = "media_player.den";
     public TextMeshPro trackTitle;
     public TextMeshPro artist;
 
     private bool _isPlaying;
     private float _currentVolume;
-    private Track _currentTrack;
+    private int _currentTrackIndex;
+    private Track? _currentTrack;
+    private Album? _currentAlbum;
+
+    public Track SetTrack { set; get; }
+    public Album SetAlbum { set; get; }
 
     void Start()
     {
         StartCoroutine(UpdateValues(delay: 0.0f));
+    }
+
+    void Update()
+    {
+        UpdateUI();
     }
 
     public IEnumerator UpdateValues(float delay = 2.5f, bool updateVolume = true)
@@ -52,7 +60,6 @@ public class SonosController : MonoBehaviour
             if (updateVolume)
                 _currentVolume = (float)attributes["volume_level"];
 
-            UpdateUI();
         }).Catch(err =>
             {
                 Debug.Log(err.Message);
@@ -61,8 +68,8 @@ public class SonosController : MonoBehaviour
 
     void UpdateUI()
     {
-        trackTitle.text = _currentTrack.title;
-        artist.text = _currentTrack.artist;
+        trackTitle.text = _currentTrack?.title ?? "";
+        artist.text = _currentTrack?.artist ?? "";
     }
 
     public void PlayPause()
@@ -71,16 +78,6 @@ public class SonosController : MonoBehaviour
         data["entity_id"] = id;
 
         HUB.Post("media_player", "media_play_pause", data);
-    }
-
-    public void AddTrack()
-    {
-        var data = new Dictionary<string, dynamic>();
-        data["entity_id"] = id;
-        data["media_content_type"] = "music";
-        data["media_content_id"] = "https://open.spotify.com/album/3IaQ0DQMIXMShbMDNepeTK?si=FT-FwhitTT-MshMvf099wA";
-
-        HUB.Post("media_player", "play_media", data);
     }
 
     public void NextTrack()
@@ -132,5 +129,30 @@ public class SonosController : MonoBehaviour
             Debug.Log(err.Message);
         });
     }
+
+    public void AddAlbum()
+    {
+
+        spotify.GetAlbum("4aawyAB9vmqN3uQ7FjRGTy").Then(res =>
+        {
+            string jsonStr = Encoding.UTF8.GetString(res.Data);
+            dynamic albumData = JValue.Parse(jsonStr);
+
+            _currentAlbum = new Album(albumData);
+            _currentTrackIndex = 0;
+            _currentTrack = _currentAlbum.tracks[0];
+
+            dynamic postData = new JObject();
+            postData.entity_id = id;
+            postData.media_content_type = "music";
+            postData.media_content_id = $"https://open.spotify.com/album/{_currentAlbum.spotifyID}";
+            // HUB.Post("media_player", "play_media", postData);
+
+        }).Catch(err =>
+        {
+            Debug.LogError(err.Message);
+        });
+    }
+
 }
 
